05d6cef542e46858a5f09b047213cd35f6e04139
sarb908 <sarbjots908@gmail.com>
2022-06-14 15:21:30 +0530

    navbar

---
+++ sprint-2/navbar-chakra/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import WithSubnavigation from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <WithSubnavigation />
+    </div>
+  );
+}
+
+export default App;
